20fbc4e46877ca957361c3563290b3466536070c
ryo219 <83750938+ryo219@users.noreply.github.com>
2021-06-24 17:56:26 +0900

    Update Dicision-Random-Comparison.py

--- Dicision-Random-Comparison.py
+++ Dicision-Random-Comparison.py
@@ -22,8 +22,7 @@ data= load_wine()
 
 
 
-
-            ##　決定木の精度測定　##            
+##　----------------- 決定木の精度測定　----------------- ##
 
 dataX = pd.DataFrame(data=data.data,columns=data.feature_names)   # 説明変数
 dataY = pd.DataFrame(data=data.target)    # 目的変数
@@ -37,7 +36,7 @@ dataY = dataY.rename(columns={0: 'class'})
 
 
 # データ分割（評価用データ＝3割）
-X_train, X_test, Y_train, Y_test = train_test_split(dataX, dataY, test_size=0.3)
+X_train, X_test, Y_train, Y_test = train_test_split(dataX, dataY, test_size=0.5)
     #print("Train-DataX below : \r\n{0}\r\n".format(X_train))
     #print("Test-DataX below : \r\n{0}\r\n".format(X_test))
     #print("Train-DataY below : \r\n{0}\r\n".format(Y_train))
@@ -51,34 +50,35 @@ clf.fit(X_train,Y_train)
 
 
 
-# 決定木の描画
-export_graphviz(clf, out_file="tree.dot", feature_names=X_train.columns, class_names=["0","1","2"], filled=True, rounded=True)
-graph = pydotplus.graph_from_dot_file(path="tree.dot")
-graph.write_png('figure-decisionTree.png')
-Image(graph.create_png())
-
-
-
+# # 決定木の描画
+#export_graphviz(clf, out_file="tree.dot", feature_names=X_train.columns, class_names=["0","1","2"], filled=True, rounded=True)
+#graph = pydotplus.graph_from_dot_file(path="tree.dot")
+#graph.write_png('figure-decisionTree.png')
+#Image(graph.create_png())
 
 
 # 精度
 print('\r\nAccuracy of DecisionTree = ', clf.score(X_test,Y_test))
 
-# 混同行列
-df = pd.DataFrame(confusion_matrix(Y_test,clf.predict(X_test).reshape(-1,1)))
-df = df.rename(columns={0: '予想(class_0)',1: '予想(class_1)',2: '予想(class_2)'}, index={0: '実際(class_0)',1: '実際(class_1)',2: '実際(class_2)'})
-df
+# # 混同行列
+#df = pd.DataFrame(confusion_matrix(Y_test,clf.predict(X_test).reshape(-1,1)))
+#df = df.rename(columns={0: '予想(class_0)',1: '予想(class_1)',2: '予想(class_2)'}, index={0: '実際(class_0)',1: '実際(class_1)',2: '実際(class_2)'})
+#df
 
 
+## --------------------------------------------------------------- ##
 
 
-            ##　ランダムフォレストの精度　##            
+##　----------------- ランダムフォレストの精度測定　----------------- ##
 
-x_train, x_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.3)
+x_train, x_test, y_train, y_test = train_test_split(dataX, dataY, test_size=0.5)
 
 model = RandomForestClassifier()
 model.fit(x_train, y_train)
 y_pred = model.predict(x_test)
 
+
 # 精度
-print('Accuracy of RandomTree = ', accuracy_score(y_pred, y_test))
+print('\r\nAccuracy of RandomTree = ', accuracy_score(y_pred, y_test))
+
+## --------------------------------------------------------------- ##
